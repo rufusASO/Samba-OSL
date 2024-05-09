493f9e83bd991d88f224eaaa4e9c957b296bb88a
julio <juliocesarsevericheorellana@gmail.com>
2024-05-08 23:12:31 -0400

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.rufus.sambagui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
+
+    opens com.rufus.sambagui to javafx.fxml;
+    exports com.rufus.sambagui;
+}
